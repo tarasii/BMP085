e3a31bb996163e92960516653ee089c195347934
tarasii <tarasii@gmail.com>
2015-01-03 20:52:12 +0200

    some un useful lines deleted

--- main.c
+++ main.c
@@ -509,44 +509,7 @@ void Init_GPIOs (void){
 //   GPIO_Init(GPIOA, &GPIO_InitStructure);
 //   GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_TIM2);
 	
-///* USART input-output temperature */	
-//	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
-//	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-//	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_40MHz;
-//  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-//  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_UP;
-
-//	GPIO_Init(GPIOA, &GPIO_InitStructure);
-//		
-//  GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
-//  GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
-
-//	USART_InitStructure.USART_BaudRate = 115200;
-//	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-//	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-//	USART_InitStructure.USART_Parity = USART_Parity_No;
-//	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-//	USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
-
-//	USART_Init(USART2, &USART_InitStructure);
-//	USART_Cmd(USART2, ENABLE);
 	
-
-//	//USART1
-//	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
-//	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-//	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_40MHz;
-//  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-//  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_UP;
-
-//	GPIO_Init(GPIOA, &GPIO_InitStructure);
-//		
-//  GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);
-//  GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);
-
-//	USART_InitStructure.USART_BaudRate = 9600;
-//	USART_Init(USART1, &USART_InitStructure);
-//	USART_Cmd(USART1, ENABLE);
 
 /* ADC input */
   GPIO_InitStructure.GPIO_Pin = IDD_MEASURE  ;                               
